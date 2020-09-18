3187b575299d3f1d70c6cde0a289065ed7b9bc19
chenyan-dfinity <yan.chen@dfinity.org>
2020-09-17 17:05:41 -0700

    update management canister interface

--- packages/agent/src/canisters/management_idl.ts
+++ packages/agent/src/canisters/management_idl.ts
@@ -1,5 +1,6 @@
 /**
- * This file is generated from the candid for asset management.
+ * This file is generated from public spec 0.10.
+ * didc bind ic.did -t js
  */
 /* tslint:disable */
 // @ts-ignore
@@ -7,25 +8,51 @@ export default ({ IDL }) => {
   const canister_id = IDL.Principal;
   const wasm_module = IDL.Vec(IDL.Nat8);
   return IDL.Service({
-    create_canister: IDL.Func([], [IDL.Record({ canister_id: canister_id })], []),
-    install_code: IDL.Func(
+    stop_canister: IDL.Func([IDL.Record({ canister_id: canister_id })], [], []),
+    start_canister: IDL.Func([IDL.Record({ canister_id: canister_id })], [], []),
+    canister_status: IDL.Func(
+      [IDL.Record({ canister_id: canister_id })],
       [
         IDL.Record({
-          mode: IDL.Variant({ install: IDL.Null, reinstall: IDL.Null, upgrade: IDL.Null }),
+          status: IDL.Variant({
+            stopped: IDL.Null,
+            stopping: IDL.Null,
+            running: IDL.Null,
+          }),
+        }),
+      ],
+      [],
+    ),
+    delete_canister: IDL.Func([IDL.Record({ canister_id: canister_id })], [], []),
+    set_controller: IDL.Func(
+      [
+        IDL.Record({
           canister_id: canister_id,
-          wasm_module: wasm_module,
-          arg: IDL.Vec(IDL.Nat8),
-          compute_allocation: IDL.Opt(IDL.Nat),
-          memory_allocation: IDL.Opt(IDL.Nat),
+          new_controller: IDL.Principal,
         }),
       ],
       [],
       [],
     ),
-    set_controller: IDL.Func(
-      [IDL.Record({ canister_id: canister_id, new_controller: IDL.Principal })],
+    install_code: IDL.Func(
+      [
+        IDL.Record({
+          arg: IDL.Vec(IDL.Nat8),
+          wasm_module: wasm_module,
+          mode: IDL.Variant({
+            reinstall: IDL.Null,
+            upgrade: IDL.Null,
+            install: IDL.Null,
+          }),
+          canister_id: canister_id,
+          memory_allocation: IDL.Opt(IDL.Nat),
+          compute_allocation: IDL.Opt(IDL.Nat),
+        }),
+      ],
       [],
       [],
     ),
+    raw_rand: IDL.Func([], [IDL.Vec(IDL.Nat8)], []),
+    create_canister: IDL.Func([], [IDL.Record({ canister_id: canister_id })], []),
   });
 };
