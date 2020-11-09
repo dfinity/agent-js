fa8d5b9f80e642808de22b3587202bc8cd231342
Andrew Wylde <andrew.wylde@dfinity.org>
2020-11-09 14:02:43 -0800

    refactor(idp): use URLSearchParams instead of window.location.search
    also check to make sure that url encoding is happening and parseable

--- packages/identity-provider/src/utils/constants.ts
+++ packages/identity-provider/src/utils/constants.ts
@@ -1,4 +1,2 @@
-export const CONSTANTS = {
-  NO_REDIRECT: 'Redirect not found in query parameters',
-  NO_PUBKEY: 'Public key not found in query parameters',
-};
+export const NO_REDIRECT = 'Redirect not found in query parameters';
+export const NO_PUBKEY = 'Public key not found in query parameters';
