0ebaba2f4e3c9ce2fd33b7216e19e5c852cfeee8
Andrew Wylde <andrew.wylde@dfinity.org>
2020-11-09 13:22:29 -0800

    feat(idp): gets public key and redirect url from query params
    throws errors when either parameter are missing, we can handle that later

--- packages/identity-provider/src/identity-provider.ts
+++ packages/identity-provider/src/identity-provider.ts
@@ -1,1 +1,26 @@
-// console.log('[identity-provider]');
+import { CONSTANTS } from './utils/constants';
+
+export function getQueryParams(): Array<Record<string, string>> {
+  const { location } = window;
+  const { search } = location;
+
+  const queryParameters = search
+    .substr(1)
+    .split('&')
+    .map(queryParameter => {
+      const [parameter, value] = queryParameter.split('=');
+      return { [parameter]: value };
+    });
+
+  const redirect = queryParameters.find(qp => qp.redirect !== undefined);
+  const publicKey = queryParameters.find(qp => qp.public_key !== undefined);
+
+  if (!redirect) {
+    throw Error(CONSTANTS.NO_REDIRECT);
+  }
+  if (!publicKey) {
+    throw Error(CONSTANTS.NO_PUBKEY);
+  }
+
+  return queryParameters;
+}
