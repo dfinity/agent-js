b39031b7a0229a5f33b3b7f8c6db01e5b1491599
Andrew Wylde <andrew.wylde@dfinity.org>
2020-11-09 14:02:43 -0800

    refactor(idp): use URLSearchParams instead of window.location.search
    also check to make sure that url encoding is happening and parseable

--- packages/identity-provider/src/identity-provider.test.ts
+++ packages/identity-provider/src/identity-provider.test.ts
@@ -1,5 +1,6 @@
-import { getQueryParams } from './identity-provider';
-import { CONSTANTS } from './utils/constants';
+import { getRequiredQueryParams } from './identity-provider';
+import * as CONSTANTS from './utils/constants';
+
 let originalLocation: Location;
 beforeAll(() => {
   originalLocation = window.location;
@@ -21,23 +22,36 @@ afterAll(() => {
   window.location = originalLocation;
 });
 describe('@dfinity/identity-provider', () => {
-  test('identity provider should pull query parameters', () => {
-    window.location.search = '?redirect=bar&public_key=fakekey';
-    const queryParams = getQueryParams();
-    expect(queryParams.length).toEqual(2);
-  });
+  describe('getQueryParams', () => {
+    test('identity provider should pull query parameters', () => {
+      window.location.search = '?redirect=bar&public_key=fakekey';
+      const queryParams = getRequiredQueryParams();
+      expect(queryParams.redirect).toEqual('bar');
+      expect(queryParams.publicKey).toEqual('fakekey');
+    });
 
-  test('getQueryParams should fail when redirect not found', () => {
-    expect(() => {
-      window.location.search = '?public_key=fakekey';
-      getQueryParams();
-    }).toThrowError(CONSTANTS.NO_REDIRECT);
-  });
+    test('getQueryParams should fail when redirect not found', () => {
+      expect(() => {
+        window.location.search = '?public_key=fakekey';
+        getRequiredQueryParams();
+      }).toThrowError(CONSTANTS.NO_REDIRECT);
+    });
 
-  test('getQueryParams should fail when public_key not found', () => {
-    expect(() => {
-      window.location.search = '?redirect=true';
-      getQueryParams();
-    }).toThrowError(CONSTANTS.NO_PUBKEY);
+    test('getQueryParams should fail when public_key not found', () => {
+      expect(() => {
+        window.location.search = '?redirect=true';
+        getRequiredQueryParams();
+      }).toThrowError(CONSTANTS.NO_PUBKEY);
+    });
+
+    test('getQueryParams should handle URL encoding', () => {
+      const fancyURL = 'http://localhost:8080/?canisterId=12345';
+      const keyParam = 'public_key=locked';
+      const urlEncodedURL = encodeURIComponent(fancyURL);
+      const searchParams = '?redirect=' + urlEncodedURL + '&' + keyParam;
+      window.location.search = searchParams;
+      const params = getRequiredQueryParams();
+      expect(params.redirect).toEqual(fancyURL);
+    });
   });
 });
