8aa7ce108515fa03a7ca4b4db0e813dbe244a58c
Andrew Wylde <andrew.wylde@dfinity.org>
2020-11-09 13:22:29 -0800

    feat(idp): gets public key and redirect url from query params
    throws errors when either parameter are missing, we can handle that later

--- packages/identity-provider/src/identity-provider.test.ts
+++ packages/identity-provider/src/identity-provider.test.ts
@@ -1,1 +1,37 @@
-test.todo('@dfinity/identity-provider has tests');
+import { getQueryParams } from './identity-provider';
+import { CONSTANTS } from './utils/constants';
+beforeAll(() => {
+  const mockResponse = jest.fn();
+  Object.defineProperty(window, 'location', {
+    value: {
+      hash: {
+        endsWith: mockResponse,
+        includes: mockResponse,
+      },
+      assign: mockResponse,
+      search: '',
+    },
+    writable: true,
+  });
+});
+describe('@dfinity/identity-provider', () => {
+  test('identity provider should pull query parameters', () => {
+    window.location.search = '?redirect=bar&public_key=fakekey';
+    const queryParams = getQueryParams();
+    expect(queryParams.length).toEqual(2);
+  });
+
+  test('getQueryParams should fail when redirect not found', () => {
+    expect(() => {
+      window.location.search = '?public_key=fakekey';
+      getQueryParams();
+    }).toThrowError(CONSTANTS.NO_REDIRECT);
+  });
+
+  test('getQueryParams should fail when public_key not found', () => {
+    expect(() => {
+      window.location.search = '?redirect=true';
+      getQueryParams();
+    }).toThrowError(CONSTANTS.NO_PUBKEY);
+  });
+});
