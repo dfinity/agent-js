d4041d026efa4d611fd640bf60bf05345bd2db4a
Andrew Wylde <andrew.wylde@dfinity.org>
2020-11-10 12:55:01 -0800

    rename public_key to login_hint

--- packages/identity-provider/src/utils/constants.ts
+++ packages/identity-provider/src/utils/constants.ts
@@ -1,2 +1,4 @@
-export const NO_REDIRECT = 'Redirect not found in query parameters';
-export const NO_PUBKEY = 'Public key not found in query parameters';
+export const NO_REDIRECT = 'Redirect URI not found in query parameters';
+export const NO_PUBKEY = 'Login hint not found in query parameters';
+export const REDIRECT_QUERY_PARAM = 'redirect_uri';
+export const LOGIN_HINT_PARAM = 'login_hint';
