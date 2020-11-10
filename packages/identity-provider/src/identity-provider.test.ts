684e70535fc3cac5ca5d1c4bf34b8f7c059a629d
Andrew Wylde <andrew.wylde@dfinity.org>
2020-11-10 12:55:01 -0800

    rename public_key to login_hint

--- packages/identity-provider/src/identity-provider.test.ts
+++ packages/identity-provider/src/identity-provider.test.ts
@@ -13,20 +13,20 @@ afterEach(() => {
 describe('@dfinity/identity-provider', () => {
   describe('getRequiredQueryParams', () => {
     test('identity provider should pull query parameters', () => {
-      const search = '?redirect_uri=bar&public_key=fakekey';
+      const search = '?redirect_uri=bar&login_hint=fakekey';
       const queryParams = getRequiredQueryParams(search);
       expect(queryParams.redirectURI).toEqual('bar');
-      expect(queryParams.publicKey).toEqual('fakekey');
+      expect(queryParams.loginHint).toEqual('fakekey');
     });
 
     test('should fail when redirect_uri not found', () => {
       expect(() => {
-        const search = '?public_key=fakekey';
+        const search = '?login_hint=fakekey';
         getRequiredQueryParams(search);
       }).toThrowError(CONSTANTS.NO_REDIRECT);
     });
 
-    test('should fail when public_key not found', () => {
+    test('should fail when login_hint not found', () => {
       expect(() => {
         const search = '?redirect_uri=true';
         getRequiredQueryParams(search);
@@ -35,13 +35,11 @@ describe('@dfinity/identity-provider', () => {
 
     test('should handle URL encoding', () => {
       const fancyURL = 'http://localhost:8080/?canisterId=12345';
-      const keyParam = 'public_key=locked';
+      const keyParam = 'login_hint=locked';
       const urlEncodedURL = encodeURIComponent(fancyURL);
       const searchParams = '?redirect_uri=' + urlEncodedURL + '&' + keyParam;
       const params = getRequiredQueryParams(searchParams);
       expect(params.redirectURI).toEqual(fancyURL);
-    });
-  });
     });
   });
 });
