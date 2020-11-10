9f33d8ec67df9220242f9f30d788bff3c1d79d4f
Andrew Wylde <andrew.wylde@dfinity.org>
2020-11-10 12:55:01 -0800

    rename public_key to login_hint

--- packages/identity-provider/src/identity-provider.ts
+++ packages/identity-provider/src/identity-provider.ts
@@ -2,23 +2,22 @@ import * as CONSTANTS from './utils/constants';
 
 interface RequiredQueryParameters {
   redirectURI: string;
-  publicKey: string;
+  loginHint: string;
 }
 
 // should look like window.location.search, i.e. ?key=value&secondKey=secondValue
 export function getRequiredQueryParams(search: string): RequiredQueryParameters {
-
   const searchParams = new URLSearchParams(search.substr(1));
 
-  const redirectURI = searchParams.get('redirect_uri');
+  const redirectURI = searchParams.get(CONSTANTS.REDIRECT_QUERY_PARAM);
   if (redirectURI === null) {
     throw Error(CONSTANTS.NO_REDIRECT);
   }
 
-  const publicKey = searchParams.get('public_key');
-  if (publicKey === null) {
+  const loginHint = searchParams.get(CONSTANTS.LOGIN_HINT_PARAM);
+  if (loginHint === null) {
     throw Error(CONSTANTS.NO_PUBKEY);
   }
 
-  return { redirectURI, publicKey };
+  return { redirectURI, loginHint };
 }
