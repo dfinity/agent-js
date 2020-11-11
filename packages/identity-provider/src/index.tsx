a26ac09eccf6d38a289c32c6c5dc7841b7e24560
Andrew Wylde <andrew.wylde@dfinity.org>
2020-11-11 09:55:48 -0800

    refactor(idp): add react & react-router to make routing/views easier
    add webpack-dev-server for ease of development

--- packages/identity-provider/src/index.tsx
+++ packages/identity-provider/src/index.tsx
@@ -1,4 +1,23 @@
-import { appendTokenParameter, getRequiredQueryParams } from './identity-provider';
+import React, { lazy, Suspense } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Home = lazy(() => import('./routes/Home'));
+const ImportKey = lazy(() => import('./routes/ImportKey'));
+
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/import' component={ImportKey} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
+
 async function _main() {
   // @TODO: remove below once we actually reassign
   // tslint:disable-next-line: prefer-const
@@ -14,6 +33,7 @@ async function _main() {
   // }
   // we've been successful
   // redirectToCanister(token);
+  render(<App />, document.body.getElementsByTagName('app').item(0));
 }
 
 _main().catch(err => {
---
+++ packages/identity-provider/src/routes/ImportKey.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ImportKey = () => {
+  return <h2>Import Key</h2>;
+};
+
+export default ImportKey;
