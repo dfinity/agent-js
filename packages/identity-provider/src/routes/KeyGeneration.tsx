4e8e79a97c9f654be337bc03ac5f6944c2d4ae3d
Andrew Wylde <2126677+codelemur@users.noreply.github.com>
2020-11-19 16:23:06 -0800

    feat(idp): implements generation of bip39 mnemonic (#74)
    * feat(idp): implements generation of bip39 mnemonic
    
    Pulls in methods/classes from auth and uses to generate on each click
    
    * show errors when importing or generating and mnemonic is invalid
    
    * simplify mnemonic submission element, sacrifice some DRYness
    
    * fix package.json
    
    * prevent default
    
    * no double confirm
    
    * refactor snackbar errors
    
    * fix eslint/prettier conflict

--- packages/identity-provider/src/routes/KeyGeneration.tsx
+++ packages/identity-provider/src/routes/KeyGeneration.tsx
@@ -1,29 +1,122 @@
-import { Container, Typography } from '@material-ui/core';
-import React, { PropsWithoutRef, useState } from 'react';
-import { RouteProps, useHistory } from 'react-router-dom';
+import { Bip39Ed25519KeyIdentity } from '@dfinity/authentication';
+import {
+  Container,
+  createStyles,
+  DialogContent,
+  DialogTitle,
+  makeStyles,
+  Snackbar,
+  Typography,
+} from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import React, { createRef, FormEvent, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Alert } from 'src/components/Alert';
 import { Button } from 'src/components/Button';
 import { Mnemonic } from 'src/components/Mnemonic';
-import { demoWordList } from 'src/utils/constants';
+import { Modal } from 'src/components/Modal';
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    submit: {
+      marginTop: theme.spacing(2),
+      alignSelf: 'flex-end',
+    },
+  }),
+);
 
 export const KeyGeneration = () => {
+  const classes = useStyles();
   const [mnemonic, setMnemonic] = useState<string[]>([]);
+  const [hasMnemonic, setHasMnemonic] = useState<boolean>(false);
+  const [keypair, setKeyPair] = useState<Bip39Ed25519KeyIdentity>();
+
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [snackError, setSnackError] = useState<Error>();
+  const _formRef = createRef<HTMLFormElement>();
 
   function generateMnemonic() {
-    // @TODO: implement bip39 mnemonic generation
-    setMnemonic(demoWordList);
+    const bip = Bip39Ed25519KeyIdentity.generate();
+    const newMnemonic = bip.getBip39Mnemonic();
+    setKeyPair(bip);
+    setMnemonic(newMnemonic.split(' '));
+    setHasMnemonic(true);
+  }
+  function handleSubmit(ev: FormEvent) {
+    ev.preventDefault();
+    const formEl = _formRef.current;
+    if (formEl) {
+      const rawInputEls = formEl.querySelectorAll<HTMLInputElement>('input[type="text"]');
+      const texts = Array.from(rawInputEls).map(ch => ch?.value);
+      const valid = texts.join(' ') === mnemonic.join(' ');
+      if (valid && keypair) {
+        setShowConfirmModal(false);
+        const { publicKey, secretKey } = keypair.getKeyPair();
+        alert(JSON.stringify({ publicKey, secretKey }));
+      } else {
+        setSnackError(Error('mnemonics do not match'));
+      }
+    }
   }
 
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackError(undefined);
+  };
   const history = useHistory();
   return (
     <Container>
-      <Typography variant={'h2'}>Generate New Key</Typography>
-      <Button variant={'outlined'} onClick={() => history.goBack()}>
+      <Typography variant='h2'>Generate New Key</Typography>
+
+      <Snackbar open={snackError !== undefined} autoHideDuration={4000}>
+        <Alert onClose={handleClose} severity='error'>
+          Error encountered: {snackError?.message}
+        </Alert>
+      </Snackbar>
+      <Button variant='outlined' onClick={() => history.goBack()}>
         Back
       </Button>
-      <Button color="primary" onClick={generateMnemonic}>
+      <Button color='primary' onClick={generateMnemonic}>
         Generate Master Key
       </Button>
-      {mnemonic.length === 24 ? <Mnemonic wordList={mnemonic} mode='read' /> : null}
+      {hasMnemonic ? <Mnemonic wordList={mnemonic} mode='read' /> : null}
+      {hasMnemonic ? (
+        <Button
+          hidden={!hasMnemonic}
+          color='secondary'
+          variant='contained'
+          onClick={() => setShowConfirmModal(true)}
+        >
+          Continue
+        </Button>
+      ) : null}
+
+      <Modal
+        fullWidth={true}
+        maxWidth='lg'
+        open={showConfirmModal}
+        onClose={() => setShowConfirmModal(false)}
+      >
+        <DialogTitle>Confirm your Mnemonic</DialogTitle>
+
+        <DialogContent>
+          <form ref={_formRef} onSubmit={handleSubmit}>
+            <Mnemonic wordList={mnemonic} mode='write' />
+            <Button
+              type='submit'
+              variant='outlined'
+              color='secondary'
+              startIcon={<SendIcon />}
+              className={classes.submit}
+            >
+              Confirm
+            </Button>
+          </form>
+        </DialogContent>
+      </Modal>
     </Container>
   );
 };
--- packages/identity-provider/src/routes/KeyImport.tsx
+++ packages/identity-provider/src/routes/KeyImport.tsx
@@ -1,44 +1,68 @@
-import { Container, Typography } from '@material-ui/core';
-import React, { useState } from 'react';
+import { Bip39Ed25519KeyIdentity } from '@dfinity/authentication';
+import * as bip39 from 'bip39';
+import { Container, Snackbar, Typography } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import React, { createRef, FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Button } from 'src/components/Button';
 import { Mnemonic } from 'src/components/Mnemonic';
+import { Alert } from 'src/components/Alert';
 
-function bip39WordValidator(word: string): boolean {
-  return word.length > 0;
-}
+const englishWords = bip39.wordlists.english;
 
 export const KeyImport = () => {
   const history = useHistory();
-  const [err, setErr] = useState('');
-
+  const [snackError, setSnackError] = useState<Error>();
+  const _formRef = createRef<HTMLFormElement>();
   const wordList = [];
   for (let i = 0; i < 24; i++) {
     wordList.push('');
   }
 
-  function handleSubmit(formEl: HTMLFormElement): void {
+  function handleSubmit(ev: FormEvent): void {
+    ev.preventDefault();
+    const formEl = _formRef.current!;
     const rawInputEls = formEl.querySelectorAll<HTMLInputElement>('input[type="text"]');
     const texts = Array.from(rawInputEls).map(ch => ch?.value);
-    const validated = texts.every(bip39WordValidator);
+    const validated = bip39.validateMnemonic(texts.join(' '), englishWords);
 
     if (validated) {
-      alert('GOOD JOB');
+      const keypair = Bip39Ed25519KeyIdentity.fromBip39Mnemonic(texts.join(' '), englishWords);
+      const { publicKey, secretKey } = keypair.getKeyPair();
+      alert(JSON.stringify({ publicKey, secretKey }));
       // @TODO: do something with the validated mnemonic
     } else {
-      setErr('one or more words in mnemonic list is malformed');
+      setSnackError(Error(' One or more words in mnemonic list is malformed'));
     }
   }
 
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackError(undefined);
+  };
+
   return (
-    <Container maxWidth={'lg'}>
-      <Typography variant={'h2'} align={'center'}>
+    <Container maxWidth='lg'>
+      <Typography variant='h2' align='center'>
         Import Existing Key
       </Typography>
-      <Button variant={'outlined'} onClick={() => history.goBack()}>
+      <Button variant='outlined' onClick={() => history.goBack()}>
         Back
       </Button>
-      <Mnemonic wordList={wordList} mode="write" onSubmit={handleSubmit} />
+      <form ref={_formRef} onSubmit={handleSubmit}>
+        <Mnemonic wordList={wordList} mode="write" />
+        <Button variant='outlined' color='secondary' startIcon={<SendIcon />} type='submit'>
+          Import
+        </Button>
+      </form>
+      <Snackbar open={snackError !== undefined}>
+        <Alert onClose={handleClose} severity="error">
+          Error encountered: {snackError?.message}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
