976fd90a83229bf2424a46e2753660e241c61e09
Benjamin Goering <ben@bengo.co>
2020-12-16 18:31:44 -0800

    update /package-lock.json

--- packages/identity-provider/src/components/Button.tsx
+++ packages/identity-provider/src/components/Button.tsx
@@ -2,16 +2,10 @@ import MatButton, { ButtonProps } from '@material-ui/core/Button';
 import React from 'react';
 
 export const Button = Object.assign(
-  (props: Pick<ButtonProps, "href"|"onClick"|"color"|"children"|"disabled"|"role"|"className">) => {
-    const { onClick, color, href, ...rootProps } = props;
-    const muiProps = { onClick, color, href };
+  (props: Pick<ButtonProps, "type"|"startIcon"|"variant"|"href"|"onClick"|"color"|"children"|"disabled"|"role"|"className">) => {
     return (
-      <span {...rootProps}>
-        <MatButton {...muiProps}>
-          {props.children}
-        </MatButton>
-      </span>
-    );
+      <MatButton {...props} />
+    )
   },
   {
     displayName: 'Button',
