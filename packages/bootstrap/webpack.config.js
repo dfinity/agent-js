bee61c1d07f8c5a95e91e688577c0a7624181e2a
Taylor Ham <taylor.ham@dfinity.org>
2020-12-18 15:37:54 -0800

    revert webpack config to prod settings, bump desktop width to 800px

--- packages/bootstrap/webpack.config.js
+++ packages/bootstrap/webpack.config.js
@@ -6,8 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  // mode: 'production',
-  mode: 'development',
+  mode: 'production',
   entry: {
     bootstrap: './src/index.ts',
     candid: './src/candid/candid.ts',
@@ -30,23 +29,23 @@ module.exports = {
     plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
     extensions: ['.tsx', '.ts', '.js'],
   },
-  // devtool: 'source-map',
-  // optimization: {
-  //   minimize: true,
-  //   minimizer: [
-  //     new TerserPlugin({
-  //       cache: true,
-  //       parallel: true,
-  //       sourceMap: true, // Must be set to true if using source-maps in production
-  //       terserOptions: {
-  //         ecma: 8,
-  //         minimize: true,
-  //         comments: false,
-  //         // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-  //       },
-  //     }),
-  //   ],
-  // },
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+          ecma: 8,
+          minimize: true,
+          comments: false,
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        },
+      }),
+    ],
+  },
   module: {
     rules: [
       {
