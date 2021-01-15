af95c4004e83c0bdedc1631c9e3d37d99083f89d
chenyan-dfinity <yan.chen@dfinity.org>
2021-01-15 13:59:56 -0800

    fix

--- e2e/node/basic/mitm.test.ts
+++ e2e/node/basic/mitm.test.ts
@@ -1,9 +1,12 @@
 import counterCanister from "../canisters/counter";
 
-test("mitm greet", async () => {
-  if (process.env["MITM"]) {
-    const { actor: counter } = await counterCanister();
-    await expect(counter.greet("counter")).rejects.toThrow(/Fail to verify certificate/);
-    expect(await counter.queryGreet("counter")).toEqual("Hullo, counter!");
-  }
+if (!process.env["MITM"]) test.only('skip mitm test', () => {
+  console.warn('Skip mitm test. Set MITM env var and start mitmdump to enable the test');
 });
+
+test("mitm greet", async () => {
+  const { actor: counter } = await counterCanister();
+  await expect(counter.greet("counter")).rejects.toThrow(/Fail to verify certificate/);
+  expect(await counter.queryGreet("counter")).toEqual("Hullo, counter!");
+});
+
