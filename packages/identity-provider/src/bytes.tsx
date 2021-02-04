998b50a2921eb83b2d8a41c33ae1bf817630ebee
Benjamin Goering <ben@bengo.co>
2021-02-04 10:52:37 -0800

    lint

--- packages/identity-provider/src/bytes.tsx
+++ packages/identity-provider/src/bytes.tsx
@@ -1,14 +1,17 @@
 /**
- * Given a UintArray of bytes, return a string of those byte hex-encoded
+ * Given a UintArray of bytes, return a string of those byte hex-encoded.
+ * @param array - array of bytes to hex-encode
  */
-export function hexEncodeUintArray(array: Uint8Array | Uint16Array) {
+export function hexEncodeUintArray(array: Uint8Array | Uint16Array): string {
   return Array.prototype.map
     .call(array, (x) => ("00" + x.toString(16)).slice(-2))
     .join("");
 }
 
-
-/** Parse a hex-string to Uint8Array of bytes */
+/**
+ * Parse a hex-string to Uint8Array of bytes.
+ * @param hex - bytes encoded as a string of concatenated hexidecimal octet strings
+ */
 export function hexToBytes(hex: string): Uint8Array {
   const octetStringsMatch = hex.match(/.{2}/g);
   if ( ! octetStringsMatch) {
