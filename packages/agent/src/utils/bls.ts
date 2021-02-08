458db91a25e7253ef2cd0b2c0d188052c3c2a49d
chenyan-dfinity <yan.chen@dfinity.org>
2021-02-08 11:23:49 -0800

    feat: BLS js library

--- packages/agent/src/utils/bls.ts
+++ packages/agent/src/utils/bls.ts
@@ -1,14 +1,15 @@
-// tslint:disable-next-line:no-var-requires
-const BLSModule = require('./bls_gen');
+import { CTX } from 'amcl-js';
 
 export class BLS {
   public static async blsVerify(pk: string, sig: string, msg: string): Promise<boolean> {
     if (!BLS.verify) {
-      const m = await BLSModule();
-      if (m._init() !== 0) {
+      const ctx = new CTX("BLS12381");
+      if (ctx.BLS.init() !== 0) {
         throw new Error('Cannot initialize BLS');
       }
-      BLS.verify = m.cwrap('verify', 'boolean', ['string', 'string', 'string']);
+      BLS.verify = (pk, sig, msg) => {
+        return ctx.BLS.core_verify(ctx.BLS.stringtobytes(sig), ctx.BLS.stringtobytes(msg), ctx.BLS.stringtobytes(pk)) == 0;
+      };
     }
     const res = BLS.verify(pk, sig, msg);
     return res;
