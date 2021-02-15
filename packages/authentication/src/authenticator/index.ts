8ed010d2195f0ac5e545feba9ad34f605778b4f2
Benjamin Goering <ben@bengo.co>
2021-02-15 09:56:21 -0800

    rm code

--- packages/authentication/src/authenticator/index.ts
+++
@@ -1,4 +0,0 @@
-export { Authenticator } from './Authenticator';
-import * as AuthenticatorModule from './Authenticator';
-export { IdentitiesIterable } from './IdentitiesIterable';
-export const authenticator = new AuthenticatorModule.Authenticator();
