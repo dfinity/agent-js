e1ada18187072d27a80fd42e2529780adb0ef066
Benjamin Goering <ben@bengo.co>
2021-02-15 09:52:42 -0800

    wip

--- packages/authentication-demo/src/authentication_demo_assets/public/canisters.ts
+++
@@ -1,6 +0,0 @@
-// @ts-expect-error because 'ic:canisters' is not resolvable without custom linker configuration in webpack.config.js
-import authDemoCanister from "ic:canisters/authentication_demo";
-import { Actor } from "@dfinity/agent";
-
-// const authentication_demo = Actor.canisterIdOf(authDemoCanister)
-export const authentication_demo: Actor = authDemoCanister;
