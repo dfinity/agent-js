eddecfb927439a26313abfe1fbf8dcc94e635eed
Hans Larsen <hans@larsen.online>
2021-02-22 18:12:39 -0800

    add a test
    Cannot test redirect unfortunately.

--- apps/identity-provider/src/routes/SignOut.tsx
+++ apps/identity-provider/src/routes/SignOut.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
-import { Redirect } from 'react-router-dom';
 
 /**
  * A `/signout?returnTo=...` route that simply returns to the original route. This is
  * for future compliance if we ever support revoking session keys.
+ * @param propReturnTo
+ * @param propReturnTo.returnTo
  */
-export default function SignOutRoute(): JSX.Element {
-  const params = new URLSearchParams(window.location.search);
-  const returnTo = params.get('returnTo');
+export default function SignOutRoute(
+  { returnTo: propReturnTo }: { returnTo?: string },
+): JSX.Element {
+  let returnTo: string | null | undefined = propReturnTo;
+  if (!returnTo) {
+    const params = new URLSearchParams(window.location.search);
+    returnTo = propReturnTo || params.get('returnTo');
+  }
 
   if (returnTo) {
-    return (
-      <Redirect to={returnTo} />
-    );
+    window.location.href = returnTo;
+    return(<></>);
   }
 
   return (
