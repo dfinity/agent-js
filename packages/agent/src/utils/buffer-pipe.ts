ad89a7fb262cf1251a8fd0262eae76e2570bcc38
Hans Larsen <hans@larsen.online>
2021-02-24 21:29:08 -0800

    chore: remove buffer-pipe as a dependency
    And re-implement it internally. This is a very trivial piece of code that
    is a step towards getting rid of Buffer itself through the codebase.
    
    Reference #222.

---
+++ packages/agent/src/utils/buffer-pipe.ts
@@ -0,0 +1,51 @@
+import { Buffer } from 'buffer/';
+
+export default class BufferPipe {
+  private _bytesRead = 0;
+  private _bytesWrote = 0;
+
+  /**
+   * @param buffer An optional buffer to start with.
+   */
+  constructor (public buffer: Buffer = Buffer.from([])) {}
+
+  /**
+   * @param num Number of bytes to read from the buffer.
+   */
+  public read(num: number): Buffer {
+    this._bytesRead += num;
+    const data = this.buffer.slice(0, num);
+    this.buffer = this.buffer.slice(num);
+    return data;
+  }
+
+  /**
+   * @param buffer A buffer to write to the pipe.
+   */
+  public write(buffer: Parameters<typeof Buffer.from>[0]): void {
+    const buf = Buffer.from(buffer);
+    this._bytesWrote += buf.length;
+    this.buffer = Buffer.concat([this.buffer, buf]);
+  }
+
+  /**
+   * @returns Whether or not there is more data to read from the buffer
+   */
+  public get end(): boolean {
+    return !this.buffer.length;
+  }
+
+  /**
+   * @returns The number of bytes read from the stream
+   */
+  public get bytesRead(): number {
+    return this._bytesRead;
+  }
+
+  /**
+   * @returns The number of bytes wrote to the stream
+   */
+  public get bytesWrote(): number {
+    return this._bytesWrote;
+  }
+}
