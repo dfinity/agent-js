67ca0a7eb9745b2f548621c365f02e21dee0a36e
Kyle Peacock <kylpeacock@gmail.com>
2021-04-01 12:16:11 -0700

    reverts testing changes

--- jest.config.base.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "@jest/types";
-
-const config: Config.InitialOptions = {
-  verbose: true,
-};
-
-export default config;
--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-import * as baseConfig from "./jest.config.base";
-
-module.exports = {
-  ...baseConfig,
-  projects: ["<rootDir>/packages/*/jest.config.js"],
-  collectCoverageFrom: ["<rootDir>/packages/*/src/**/*.{ts,tsx}"],
-};
