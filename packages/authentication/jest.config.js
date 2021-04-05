fa1576cbf647c250e528990f8cbd98d570f37e1f
Kyle Peacock <kylpeacock@gmail.com>
2021-04-05 15:08:03 -0700

    chore: configure repo to run jest from root level (#336)

--- packages/authentication/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bail: false,
-  setupFiles: ['./test-setup'],
-  setupFilesAfterEnv: ['jest-expect-message'],
-  testPathIgnorePatterns: ['/node_modules/', '/out/', '\\.js$'],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-};
