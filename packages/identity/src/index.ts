f325bfacb758a2d601b30ab59f3f19289b7419b8
Islam El-Ashi <islam.elashi@dfinity.org>
2021-04-15 20:52:06 +0200

    Export Delegation and SignedDelegation

--- packages/identity/src/index.ts
+++ packages/identity/src/index.ts
@@ -1,6 +1,6 @@
 export { Ed25519KeyIdentity, Ed25519PublicKey } from './identity/ed25519';
 export { Bip39Ed25519KeyIdentity } from './identity/bip39';
-export { DelegationIdentity, DelegationChain } from './identity/delegation';
+export { Delegation, DelegationIdentity, DelegationChain, SignedDelegation } from './identity/delegation';
 export { WebAuthnIdentity } from './identity/webauthn';
 
 
