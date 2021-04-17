dcdaefda7a29d59cc3389af90bcf152639306306
Linwei Shang <linwei.shang@dfinity.org>
2021-04-16 17:31:22 -0700

    Add files from ledger-dfinity js
    Should be removed when the npm package is available

---
+++ packages/identity/src/ledger-dfinity/types.ts
@@ -0,0 +1,36 @@
+export interface ResponseBase {
+  errorMessage: string;
+  returnCode: number;
+}
+
+export interface ResponseAddress extends ResponseBase {
+  publicKey: Buffer;
+  address: string;
+  addressText: string;
+}
+
+export interface ResponseVersion extends ResponseBase {
+  testMode: boolean;
+  major: number;
+  minor: number;
+  patch: number;
+  deviceLocked: boolean;
+  targetId: string;
+}
+
+export interface ResponseAppInfo extends ResponseBase {
+  appName: string;
+  appVersion: string;
+  flagLen: number;
+  flagsValue: number;
+  flagRecovery: boolean;
+  flagSignedMcuCode: boolean;
+  flagOnboarded: boolean;
+  flagPINValidated: boolean;
+}
+
+export interface ResponseSign extends ResponseBase {
+  preSignHash: Buffer,
+  signatureRS: Buffer,
+  signatureDER: Buffer,
+}
