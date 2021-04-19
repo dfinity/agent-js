a9ff013867a32ad27a7d0ffbf4bec000eba2850b
Linwei Shang <linwei.shang@dfinity.org>
2021-04-18 17:33:12 -0700

    Export classes in ledger

--- packages/identity/src/index.ts
+++ packages/identity/src/index.ts
@@ -2,4 +2,4 @@ export { Ed25519KeyIdentity, Ed25519PublicKey } from './identity/ed25519';
 export { Bip39Ed25519KeyIdentity } from './identity/bip39';
 export { Delegation, DelegationIdentity, DelegationChain, SignedDelegation } from './identity/delegation';
 export { WebAuthnIdentity } from './identity/webauthn';
-export { LedgerIdentity } from './identity/ledger';
+export { LedgerIdentity, LedgerManager, Secp256k1PublicKey } from './identity/ledger';
