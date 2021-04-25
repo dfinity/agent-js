bea7576d38651ed60dcbf04811eda8dabdb5990b
Hans Larsen <hans@larsen.online>
2021-04-24 21:42:05 -0700

    WIP

--- packages/identity/src/index.ts
+++ packages/identity/src/index.ts
@@ -2,3 +2,5 @@ export { Ed25519KeyIdentity, Ed25519PublicKey } from './identity/ed25519';
 export { Bip39Ed25519KeyIdentity } from './identity/bip39';
 export { Delegation, DelegationIdentity, DelegationChain, SignedDelegation } from './identity/delegation';
 export { WebAuthnIdentity } from './identity/webauthn';
+
+
