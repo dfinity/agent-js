1c2d191e98955177dccd84c6b829cf3f25c790bc
Hans Larsen <hans@larsen.online>
2021-05-04 08:42:58 -0700

    chore: create a new sw-cert app for the service worker (#399)
    These are just files that build to a service worker that gets installed and ALWAYS return "Hello World". This is a vague copy of Dom's code, with some comments removed and refactoring in. A lot still need to happen but this should get us started.
    
    The Service Worker right now just does the same as the icx-proxy, but in browser.

---
+++ apps/sw-cert/src/sw/sw.ts
@@ -0,0 +1,28 @@
+import { sha256 } from 'js-sha256';
+import { handleRequest } from './http_request';
+
+declare const self: ServiceWorkerGlobalScope;
+
+const DEBUG = true;
+
+// Always install updated SW immediately
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+// Intercept and proxy all fetch requests made by the browser or DOM on this scope.
+self.addEventListener('fetch', (event: FetchEvent) => {
+  try {
+    event.respondWith(handleRequest(event.request))
+  } catch (e) {
+    console.error(e.message || e.toString());
+    if (DEBUG) {
+      return event.respondWith(
+        new Response(e.message || e.toString(), {
+          status: 501,
+        }),
+      )
+    }
+    event.respondWith(new Response('Internal Error', { status: 502 }));
+  }
+});
---
+++ apps/sw-cert/webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    bundle: path.join(__dirname, 'src/index.ts'),
+  },
+  mode: 'production',
+  target: 'web',
+  output: {
+    path: path.join(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      process: "process/browser"
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+      "assert": require.resolve("assert/"),
+      "buffer": require.resolve("buffer/"),
+      "events": require.resolve("events/"),
+      "stream": require.resolve("stream-browserify/"),
+      "util": require.resolve("util/"),
+    },
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      chunks: [
+        "bundle",
+      ],
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: [require.resolve('buffer/'), 'Buffer'],
+      process: require.resolve('process/browser'),
+    }),
+  ],
+};
