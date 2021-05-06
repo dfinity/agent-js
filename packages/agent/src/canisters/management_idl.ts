982c92eedf4e1e530e1818ef158badc43219d037
megrogan <megrogan@gmail.com>
2021-05-06 14:14:13 +0100

    feat: Use full IC Management IDL

--- packages/agent/src/canisters/management_idl.ts
+++ packages/agent/src/canisters/management_idl.ts
@@ -5,37 +5,108 @@
 // @ts-ignore
 export default ({ IDL }) => {
   const canister_id = IDL.Principal;
-  const wasm_module = IDL.Vec(IDL.Nat8);
-  const CanisterSettings = IDL.Record({
-    compute_allocation: IDL.Opt(IDL.Nat),
-    memory_allocation: IDL.Opt(IDL.Nat),
+  const definite_canister_settings = IDL.Record({
+    'controller' : IDL.Principal,
+    'freezing_threshold' : IDL.Nat,
+    'memory_allocation' : IDL.Nat,
+    'compute_allocation' : IDL.Nat,
   });
+  const canister_settings = IDL.Record({
+    'controller' : IDL.Opt(IDL.Principal),
+    'freezing_threshold' : IDL.Opt(IDL.Nat),
+    'memory_allocation' : IDL.Opt(IDL.Nat),
+    'compute_allocation' : IDL.Opt(IDL.Nat),
+  });
+  const wasm_module = IDL.Vec(IDL.Nat8);
   return IDL.Service({
-    provisional_create_canister_with_cycles: IDL.Func(
-      [
-        IDL.Record({ amount: IDL.Opt(IDL.Nat), settings: IDL.Opt(CanisterSettings) }),
-      ], [
-        IDL.Record({ canister_id: canister_id }),
-      ],
-      [],
-    ),
-    create_canister: IDL.Func([], [IDL.Record({ canister_id: canister_id })], []),
-    install_code: IDL.Func(
-      [
-        IDL.Record({
-          mode: IDL.Variant({ install: IDL.Null, reinstall: IDL.Null, upgrade: IDL.Null }),
-          canister_id: canister_id,
-          wasm_module: wasm_module,
-          arg: IDL.Vec(IDL.Nat8),
-        }),
-      ],
-      [],
-      [],
-    ),
-    set_controller: IDL.Func(
-      [IDL.Record({ canister_id: canister_id, new_controller: IDL.Principal })],
-      [],
-      [],
-    ),
+    'canister_status' : IDL.Func(
+        [IDL.Record({ 'canister_id' : canister_id })],
+        [
+          IDL.Record({
+            'status' : IDL.Variant({
+              'stopped' : IDL.Null,
+              'stopping' : IDL.Null,
+              'running' : IDL.Null,
+            }),
+            'memory_size' : IDL.Nat,
+            'cycles' : IDL.Nat,
+            'settings' : definite_canister_settings,
+            'module_hash' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+          }),
+        ],
+        [],
+      ),
+    'create_canister' : IDL.Func(
+        [IDL.Record({ 'settings' : IDL.Opt(canister_settings) })],
+        [IDL.Record({ 'canister_id' : canister_id })],
+        [],
+      ),
+    'delete_canister' : IDL.Func(
+        [IDL.Record({ 'canister_id' : canister_id })],
+        [],
+        [],
+      ),
+    'deposit_cycles' : IDL.Func(
+        [IDL.Record({ 'canister_id' : canister_id })],
+        [],
+        [],
+      ),
+    'install_code' : IDL.Func(
+        [
+          IDL.Record({
+            'arg' : IDL.Vec(IDL.Nat8),
+            'wasm_module' : wasm_module,
+            'mode' : IDL.Variant({
+              'reinstall' : IDL.Null,
+              'upgrade' : IDL.Null,
+              'install' : IDL.Null,
+            }),
+            'canister_id' : canister_id,
+          }),
+        ],
+        [],
+        [],
+      ),
+    'provisional_create_canister_with_cycles' : IDL.Func(
+        [
+          IDL.Record({
+            'settings' : IDL.Opt(canister_settings),
+            'amount' : IDL.Opt(IDL.Nat),
+          }),
+        ],
+        [IDL.Record({ 'canister_id' : canister_id })],
+        [],
+      ),
+    'provisional_top_up_canister' : IDL.Func(
+        [IDL.Record({ 'canister_id' : canister_id, 'amount' : IDL.Nat })],
+        [],
+        [],
+      ),
+    'raw_rand' : IDL.Func([], [IDL.Vec(IDL.Nat8)], []),
+    'start_canister' : IDL.Func(
+        [IDL.Record({ 'canister_id' : canister_id })],
+        [],
+        [],
+      ),
+    'stop_canister' : IDL.Func(
+        [IDL.Record({ 'canister_id' : canister_id })],
+        [],
+        [],
+      ),
+    'uninstall_code' : IDL.Func(
+        [IDL.Record({ 'canister_id' : canister_id })],
+        [],
+        [],
+      ),
+    'update_settings' : IDL.Func(
+        [
+          IDL.Record({
+            'canister_id' : IDL.Principal,
+            'settings' : canister_settings,
+          }),
+        ],
+        [],
+        [],
+      ),
   });
 };
