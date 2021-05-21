d5ba16d7130b39855131cc3f56013fbdd71829bc
Hamish Peebles <hamishpeebles@gmail.com>
2021-05-21 09:52:41 +0100

    Update the OpenChat canisterId as it has been moved to its own subnet

--- apps/sw-cert/src/sw/http_request.ts
+++ apps/sw-cert/src/sw/http_request.ts
@@ -13,8 +13,8 @@ const hostnameCanisterIdMap: Record<string, [string, string]> = {
   'identity.ic0.app': ['rdmx6-jaaaa-aaaaa-aaadq-cai', 'ic0.app'],
   'nns.ic0.page': ['qoctq-giaaa-aaaaa-aaaea-cai', 'ic0.page'],
   'nns.ic0.app': ['qoctq-giaaa-aaaaa-aaaea-cai', 'ic0.app'],
-  'chat.ic0.page': ['l4kjv-xyaaa-aaaae-qaaba-cai', 'ic0.page'],
-  'chat.ic0.app': ['l4kjv-xyaaa-aaaae-qaaba-cai', 'ic0.app'],
+  'chat.ic0.page': ['7rzzy-aaaaa-aaaaf-aaaaq-cai', 'ic0.page'],
+  'chat.ic0.app': ['7rzzy-aaaaa-aaaaf-aaaaq-cai', 'ic0.app'],
   'dscvr.ic0.page': ['h5aet-waaaa-aaaab-qaamq-cai', 'ic0.page'],
   'dscvr.ic0.app': ['h5aet-waaaa-aaaab-qaamq-cai', 'ic0.app'],
 };
