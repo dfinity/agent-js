6e8c64cf07c7722aafbf52351eb0f19fcb954ff0
Hans Larsen <hans@larsen.online>
2021-05-27 09:52:29 -0700

    chore: re-add dscvr.ic0.app (#434)

--- apps/sw-cert/src/sw/http_request.ts
+++ apps/sw-cert/src/sw/http_request.ts
@@ -12,6 +12,7 @@ import * as pako from 'pako';
 const hostnameCanisterIdMap: Record<string, [string, string]> = {
   'identity.ic0.app': ['rdmx6-jaaaa-aaaaa-aaadq-cai', 'ic0.app'],
   'nns.ic0.app': ['qoctq-giaaa-aaaaa-aaaea-cai', 'ic0.app'],
+  'dscvr.ic0.app': ['h5aet-waaaa-aaaab-qaamq-cai', 'ic0.page'],
 };
 
 const shouldFetchRootKey: boolean = !!process.env.FORCE_FETCH_ROOT_KEY || false;
