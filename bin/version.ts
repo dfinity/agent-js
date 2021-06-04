77f175f7a2e178c104933dc3239eacd2e9fcbbd4
Kyle Peacock <kylpeacock@gmail.com>
2021-06-04 12:54:11 -0700

    script in working order

---
+++ bin/version.ts
@@ -0,0 +1,113 @@
+import fs from 'fs';
+import path from 'path';
+import yargs from 'yargs';
+import { exec } from 'child_process';
+
+console.time('script duration');
+console.log('Updating package versions...');
+
+// Manage CLI options
+const argv = yargs
+  .command('patch', 'Increments up a patch version for all packages')
+  .command('minor', 'Increments a minor version for all packages')
+  .command('major', 'Increments a major version for all packages')
+  .option('tag', {
+    alias: 't',
+    description: 'Set the tag that the packages should be published to',
+    type: 'string',
+    default: 'latest',
+  })
+  .default({ tag: 'set-version' })
+  .help()
+  .alias('help', 'h').argv;
+
+// Infer info about workspaces from package.json
+const rootPackage = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '..', 'package.json')).toString(),
+);
+
+if (!rootPackage.name) throw new Error("Couldn't find root package.json");
+
+const baseVersion = rootPackage.version;
+
+const newVersion = (() => {
+  // eslint-disable-next-line
+  let [major, minor, patch, ...rest] = baseVersion.split('.');
+  if (argv._.includes('patch')) {
+    patch = Number(patch) + 1;
+  } else if (argv._.includes('minor')) {
+    minor = Number(minor) + 1;
+  } else if (argv._.includes('major')) {
+    major = Number(major) + 1;
+  } else {
+    // else use the immediately following argument
+    return argv._[0];
+  }
+  return [major, minor, patch, ...rest].join('.');
+})();
+
+console.log('New version will be: ' + newVersion);
+
+const workspaces: string[] = rootPackage.workspaces?.packages;
+
+const packages = workspaces
+  .filter(workspace => workspace.includes('packages'))
+  .map(packagePath => packagePath.replace('packages', '@dfinity'));
+
+const promises = workspaces.map(workspace => {
+  return new Promise((resolve, reject) => {
+    const packagePath = path.resolve(__dirname, '..', workspace, 'package.json');
+    const json = fs.readFile(packagePath, (err, buffer) => {
+      if (err) reject(err);
+      const json = JSON.parse(buffer.toString());
+      json.version = newVersion;
+      if (json.peerDependencies) {
+        json.peerDependencies = updateDeps(json.peerDependencies);
+      }
+      if (json.dependencies) {
+        json.dependencies = updateDeps(json.dependencies);
+      }
+      if (json.devDependencies) {
+        json.devDependencies = updateDeps(json.devDependencies);
+      }
+
+      fs.writeFile(packagePath, JSON.stringify(json), {}, () => {
+        resolve('success');
+      });
+    });
+  });
+});
+function updateDeps(dependencies: Record<string, string>) {
+  for (const dep in dependencies) {
+    if (Object.prototype.hasOwnProperty.call(dependencies, dep)) {
+      if (packages.includes(dep)) {
+        dependencies[dep] = '^' + newVersion.toString();
+      }
+    }
+  }
+  return dependencies;
+}
+
+Promise.all(promises)
+  .then(() => {
+    // Prettier format the modified package.json files
+    exec(`npm run prettier:format`, error => {
+      if (error) {
+        throw new Error(JSON.stringify(error));
+      }
+
+      // Publish packages to npm using provided tag
+      exec(`npm publish --workspaces ${argv.tag ? `--tag ${argv.tag}` : ''}`, error => {
+        if (error) {
+          throw new Error(JSON.stringify(error));
+        }
+        // wrap up
+        console.log('success!');
+        console.timeEnd('script duration');
+      });
+    });
+  })
+  .catch(err => {
+    console.timeEnd('script duration');
+    throw new Error(err);
+  });
