e1730742ad9e9a50fa81bad441a8c0e5fb9a8dab
Kyle Peacock <kylpeacock@gmail.com>
2021-06-04 15:47:55 -0700

    updating script with experiments from personal repo

--- bin/version.ts
+++ bin/version.ts
@@ -11,18 +11,10 @@ const argv = yargs
   .command('patch', 'Increments up a patch version for all packages')
   .command('minor', 'Increments a minor version for all packages')
   .command('major', 'Increments a major version for all packages')
-  .option('tag', {
-    alias: 't',
-    description: 'Set the tag that the packages should be published to',
-    type: 'string',
-    default: 'latest',
-  })
-  .option('publish', {
-    description: 'Should the packages be published?',
-    type: 'boolean',
-    default: false,
-  })
-  .default({ tag: 'set-version' })
+  .example('patch', 'npm run version -- patch')
+  .example('minor', 'npm run version -- minor')
+  .example('major', 'npm run version -- major')
+  .example('custom', 'npm run version -- 0.9.0-beta.1')
   .help()
   .alias('help', 'h').argv;
 
@@ -45,18 +37,18 @@ const newVersion = (() => {
   } else if (argv._.includes('major')) {
     major = Number(major) + 1;
   } else {
-    // else use the immediately following argument
-    return argv._[0];
+    // else use the first argument
+    return argv._[0].toString();
   }
   return [major, minor, patch, ...rest].join('.');
 })();
 
-rootPackage.version = newVersion;
-
 console.log('New version will be: ' + newVersion);
 
+// Read workspaces from root package.json
 const workspaces: string[] = rootPackage.workspaces?.packages;
 
+// Identify packages in `/packages directory
 const packages = workspaces
   .filter(workspace => workspace.includes('packages'))
   .map(packagePath => packagePath.replace('packages', '@dfinity'));
@@ -65,7 +57,10 @@ workspaces.forEach(async workspace => {
   const packagePath = path.resolve(__dirname, '..', workspace, 'package.json');
   const json = JSON.parse(fs.readFileSync(packagePath).toString());
 
+  // Set version for package
   json.version = newVersion;
+
+  // Update references to any packages that being updated
   if (json.peerDependencies) {
     json.peerDependencies = updateDeps(json.peerDependencies);
   }
@@ -76,13 +71,14 @@ workspaces.forEach(async workspace => {
     json.devDependencies = updateDeps(json.devDependencies);
   }
 
+  // Write file
   fs.writeFileSync(packagePath, JSON.stringify(json));
 });
 function updateDeps(dependencies: Record<string, string>) {
   for (const dep in dependencies) {
     if (Object.prototype.hasOwnProperty.call(dependencies, dep)) {
       if (packages.includes(dep)) {
-        dependencies[dep] = '^' + newVersion.toString();
+        dependencies[dep] = '^' + newVersion;
       }
     }
   }
@@ -97,23 +93,7 @@ exec(`npm run prettier:format`, error => {
     throw new Error(JSON.stringify(error));
   }
 
-  if (argv.publish) {
-    // Publish packages to npm using provided tag
-    console.log('Publishing packages to npm with tag' + argv.tag);
-    exec(
-      `npm publish --workspaces${argv.tag ? ` --tag ${argv.tag}` : ''} --access-public`,
-      error => {
-        if (error) {
-          throw new Error(JSON.stringify(error));
-        }
-        // wrap up
-        console.log('success!');
-        console.timeEnd('script duration');
-      },
-    );
-  } else {
-    // wrap up
-    console.log('success!');
-    console.timeEnd('script duration');
-  }
+  // wrap up
+  console.log('success!');
+  console.timeEnd('script duration');
 });
