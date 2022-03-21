9572a5aedc3dd6418029af67a3e495f052d81ffb
Kyle Peacock <kylpeacock@gmail.com>
2022-03-21 15:28:40 -0700

    timeout

--- e2e/node/basic/mitm.test.ts
+++ e2e/node/basic/mitm.test.ts
@@ -8,6 +8,7 @@ if (!process.env['MITM']) {
   mitmTest = test.skip;
 }
 
+jest.setTimeout(30000);
 mitmTest('mitm greet', async () => {
   const { actor: counter } = await counterCanister();
   await expect(counter.greet('counter')).rejects.toThrow(/Fail to verify certificate/);
