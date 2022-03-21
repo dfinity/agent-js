c60737053aa9dbd1539e475371e836f974ca919f
Kyle Peacock <kylpeacock@gmail.com>
2022-03-21 15:18:15 -0700

    breaking out mitm

--- e2e/node/basic/counter.test.ts
+++ e2e/node/basic/counter.test.ts
@@ -4,14 +4,11 @@
 import counterCanister from '../canisters/counter';
 
 jest.setTimeout(30000);
-jest.retryTimes(3);
 describe('counter', () => {
   it('should greet', async () => {
     const { actor: counter } = await counterCanister();
     try {
-      const result = await counter.greet('counter');
-      console.log('greet result: ', result);
-      expect(result).toEqual('Hello, counter!');
+      expect(await counter.greet('counter')).toEqual('Hello, counter!');
     } catch (error) {
       console.error(error);
     }
@@ -19,14 +16,13 @@ describe('counter', () => {
   it('should increment', async () => {
     const { actor: counter } = await counterCanister();
     try {
-      console.log('value: ', await counter.read());
       expect(Number(await counter.read())).toEqual(0);
       await counter.inc();
+      expect(Number(await counter.read())).toEqual(1);
+      await counter.inc();
+      expect(Number(await counter.read())).toEqual(2);
     } catch (error) {
       console.error(error);
     }
-    // expect(Number(await counter.read())).toEqual(1);
-    // await counter.inc();
-    // expect(Number(await counter.read())).toEqual(2);
   });
 });
