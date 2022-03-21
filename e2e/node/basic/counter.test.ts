5a4f9c48d6865fa1789f12116ad8af31ded9ebc3
Kyle Peacock <kylpeacock@gmail.com>
2022-03-21 14:46:49 -0700

    error handling

--- e2e/node/basic/counter.test.ts
+++ e2e/node/basic/counter.test.ts
@@ -3,17 +3,26 @@
  */
 import counterCanister from '../canisters/counter';
 
-describe('counter', async () => {
-  const { actor: counter } = await counterCanister();
+describe('counter', () => {
   it('should greet', async () => {
-    const result = await counter.greet('counter');
-    console.log('greet result: ', result);
-    expect(result).toEqual('Hello, counter!');
+    const { actor: counter } = await counterCanister();
+    try {
+      const result = await counter.greet('counter');
+      console.log('greet result: ', result);
+      expect(result).toEqual('Hello, counter!');
+    } catch (error) {
+      console.error(error);
+    }
   });
   it('should increment', async () => {
-    console.log('value: ', await counter.read());
-    expect(Number(await counter.read())).toEqual(0);
-    await counter.inc();
+    const { actor: counter } = await counterCanister();
+    try {
+      console.log('value: ', await counter.read());
+      expect(Number(await counter.read())).toEqual(0);
+      await counter.inc();
+    } catch (error) {
+      console.error(error);
+    }
     // expect(Number(await counter.read())).toEqual(1);
     // await counter.inc();
     // expect(Number(await counter.read())).toEqual(2);
