9b663c69243e7c61d42fcc8de6f992d3b15f2bdf
megrogan <megrogan@gmail.com>
2022-03-24 14:39:18 +0000

    feat: Use full IC Management IDL (#406)
    * feat: Use full IC Management IDL
    
    * Assert fn prov...with_cycles has type ActorMethod
    
    Co-authored-by: Kyle Peacock <kylpeacock@gmail.com>

--- packages/agent/src/canisters/management.ts
+++ packages/agent/src/canisters/management.ts
@@ -1,45 +1,16 @@
-import { Actor, ActorMethod, ActorSubclass, CallConfig } from '../actor';
+import { Actor, ActorSubclass, CallConfig } from '../actor';
 import { Principal } from '@dfinity/principal';
 import managementCanisterIdl from './management_idl';
+import _SERVICE from './management_service';
 
-export interface CanisterSettings {
-  controller: [] | [Principal];
-  compute_allocation: [] | [bigint];
-  memory_allocation: [] | [bigint];
-  freezing_threshold: [] | [bigint];
-}
-
-/* tslint:disable */
-export interface ManagementCanisterRecord {
-  provisional_create_canister_with_cycles: ActorMethod<
-    [
-      {
-        amount: [] | [number];
-        settings: [] | [CanisterSettings];
-      },
-    ],
-    { canister_id: Principal }
-  >;
-  install_code: ActorMethod<
-    [
-      {
-        mode: { install: null } | { reinstall: null } | { upgrade: null };
-        canister_id: Principal;
-        wasm_module: number[];
-        arg: number[];
-      },
-    ],
-    void
-  >;
-}
-/* tslint:enable */
+export type ManagementCanisterRecord = _SERVICE;
 
 /**
- * Create a management canister actor.
+ * Create a management canister actor
  * @param config
  */
 export function getManagementCanister(config: CallConfig): ActorSubclass<ManagementCanisterRecord> {
-  function transform(methodName: string, args: unknown[], callConfig: CallConfig) {
+  function transform(_methodName: string, args: unknown[], _callConfig: CallConfig) {
     const first = args[0] as any;
     let effectiveCanisterId = Principal.fromHex('');
     if (first && typeof first === 'object' && first.canister_id) {
