60dc6109bf36485b41b88d9c118fcd54da52f40d
Kyle Peacock <kylpeacock@gmail.com>
2022-06-29 15:16:24 -0700

    feat: jwk support for ecdsa

--- packages/identity/src/identity/ecdsa.ts
+++ packages/identity/src/identity/ecdsa.ts
@@ -1,10 +1,22 @@
-import { DerEncodedPublicKey, KeyPair, PublicKey, Signature, SignIdentity } from '@dfinity/agent';
+import { DerEncodedPublicKey, PublicKey, Signature, SignIdentity } from '@dfinity/agent';
+import { toHexString } from 'src/buffer';
+// import { randomBytes } from 'tweetnacl';
 import { ED25519_OID, unwrapDER, wrapDER } from './der';
 
-type extractable = boolean;
-type CryptoKeyOptions = [extractable, KeyUsage[]];
+type CryptoKeyOptions = { extractable: boolean; keyUsages: KeyUsage[] };
 
-export class EcdsaPublicKey implements PublicKey {
+export class ExtractrableKeyError extends Error {
+  constructor(public readonly message: string) {
+    super(message);
+    Object.setPrototypeOf(this, ExtractrableKeyError.prototype);
+  }
+}
+
+interface CryptoPublicKey extends CryptoKey {
+  toDer(): DerEncodedPublicKey;
+}
+
+export class ECDSAPublicKey implements CryptoPublicKey {
   private static derEncode(publicKey: ArrayBuffer): DerEncodedPublicKey {
     // TODO - replace placeholder DER logic
     return wrapDER(publicKey, ED25519_OID).buffer as DerEncodedPublicKey;
@@ -13,36 +25,115 @@ export class EcdsaPublicKey implements PublicKey {
     const unwrapped = unwrapDER(key, ED25519_OID);
     return unwrapped;
   }
-  private readonly key: CryptoKey;
-  private readonly derKey: DerEncodedPublicKey;
+
+  public algorithm: CryptoKey['algorithm'];
+  public extractable: CryptoKey['extractable'];
+  public type: CryptoKey['type'];
+  public usages: CryptoKey['usages'];
+
+  private readonly rawKey: ArrayBuffer | undefined;
+  private readonly jwk: JsonWebKey | undefined;
+  private readonly derKey: DerEncodedPublicKey | undefined;
+
+  // /**
+  //  *
+  //  * @param {ArrayBuffer} rawKey
+  //  * @param {CryptoKeyOptions} cryptoKeyOptions
+  //  * @returns
+  //  */
+  public static async fromJWK(
+    jwk: JsonWebKey,
+    cryptoKeyOptions?: CryptoKeyOptions,
+  ): Promise<ECDSAPublicKey> {
+    jwk;
+
+    const { extractable = true, keyUsages = ['sign', 'verify'] } = cryptoKeyOptions ?? {};
+    const key = await crypto.subtle.importKey('jwk', jwk, 'ecdsa', extractable, keyUsages);
+
+    const rawKey = await crypto.subtle.exportKey('raw', key);
+
+    return new ECDSAPublicKey(key, rawKey);
+  }
+
+  // public async fromDer(
+  //   derKey: DerEncodedPublicKey,
+  //   cryptoKeyOptions?: CryptoKeyOptions,
+  // ): Promise<ECDSAPublicKey> {
+  //   const [extractable = false, keyUsage = ['sign', 'verify']] = cryptoKeyOptions ?? [];
+  //   const rawKey = ECDSAPublicKey.derDecode(derKey);
+  //   const key = await SubtleCrypto.prototype.importKey(
+  //     'raw',
+  //     rawKey,
+  //     'ecdsa',
+  //     extractable,
+  //     keyUsage,
+  //   );
+  //   return new ECDSAPublicKey(rawKey, key, [extractable, keyUsage]);
+  // }
 
   /**
-   *
-   * @param {ArrayBuffer} rawKey
-   * @param {CryptoKeyOptions} cryptoKeyOptions
+   * Generates a new ECDSAPublicKey using the ECDSA P-384 curve
+   * @param {CryptoKeyOptions} cryptoKeyOptions for extractable flag and KeyUsages
+   * @param {boolean} CryptoKeyOptions.extractable
    * @returns
    */
-  public async fromRaw(
-    rawKey: ArrayBuffer,
-    cryptoKeyOptions?: CryptoKeyOptions,
-  ): Promise<EcdsaPublicKey> {
-    const configurableOptions = cryptoKeyOptions ?? [false, ['sign', 'verify']];
-    const options = ['raw', rawKey, 'ecdsa', ...configurableOptions];
-    const key = await SubtleCrypto.prototype.importKey.call(this, ...options);
-    return new EcdsaPublicKey(rawKey, key);
+  public static async generate(): Promise<ECDSAPublicKey> {
+    const params = {
+      name: 'ECDSA',
+      namedCurve: 'P-384',
+    };
+
+    const keyPair = await crypto.subtle.generateKey(params, false, ['sign', 'verify']);
+
+    const publicKey = keyPair.publicKey;
+    const jwk = await crypto.subtle.exportKey('jwk', publicKey);
+    const rawKey = await crypto.subtle.exportKey('raw', publicKey);
+
+    return new ECDSAPublicKey(publicKey, rawKey, jwk);
   }
 
-  public static fromDer(derKey: DerEncodedPublicKey): EcdsaPublicKey {
-    return new EcdsaPublicKey(this.derDecode(derKey));
-  }
+  private constructor(key: CryptoKey, rawKey?: ArrayBuffer, jwk?: JsonWebKey) {
+    this.rawKey = rawKey;
 
-  private constructor(rawKey: ArrayBuffer, key: CryptoKey) {
-    this.key = key;
-    this.derKey = EcdsaPublicKey.derEncode(rawKey);
+    if (rawKey) {
+      this.derKey = ECDSAPublicKey.derEncode(rawKey);
+    }
+    if (jwk) {
+      this.jwk = jwk;
+    }
+
+    // Copy attributes from key
+    this.algorithm = key.algorithm;
+    this.extractable = key.extractable;
+    this.type = key.type;
+    this.usages = key.usages;
   }
 
   public toDer(): DerEncodedPublicKey {
+    if (!this.derKey) {
+      throw new ExtractrableKeyError(
+        'Error: could not export key for DER-encoding. If you need to extract this key, set CryptoKeyOptions.extractable to true during creation.',
+      );
+    }
     return this.derKey;
   }
+
+  public toRaw(): ArrayBuffer {
+    if (!this.rawKey) {
+      throw new ExtractrableKeyError(
+        'Error: could not export raw-encoded key. If you need to extract this key, set CryptoKeyOptions.extractable to true during creation.',
+      );
+    }
+    return this.rawKey;
+  }
+
+  public toJwk(): JsonWebKey {
+    if (!this.jwk) {
+      throw new ExtractrableKeyError(
+        'Error: could not export jwk-encoded key. If you need to extract this key, set CryptoKeyOptions.extractable to true during creation.',
+      );
+    }
+    return this.jwk;
+  }
 }
-export class EcdsaKeyIdentity {}
+export class ECDSAKeyIdentity {}
