bdf6cc690a823d40a8e5b7e3b0791853706fa5b1
Kyle Peacock <kylpeacock@gmail.com>
2022-07-20 14:40:28 -0700

    backgorund task

--- e2e/browser/cypress.config.js
+++ e2e/browser/cypress.config.js
@@ -3,6 +3,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
