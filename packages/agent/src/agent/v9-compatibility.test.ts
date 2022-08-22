418fc5baef70cc664fb736a4035c783715f6d398
Kyle Peacock <kylpeacock@gmail.com>
2022-08-22 16:10:09 -0700

    wip

--- packages/agent/src/agent/v9-compatibility.test.ts
+++ packages/agent/src/agent/v9-compatibility.test.ts
@@ -1,18 +1,29 @@
-import { getNFTActor } from '@psychedelic/dab-js';
+import { getNFTActor, LegacyHttpAgent } from '@psychedelic/dab-js';
+import { Principal } from '@dfinity/principal';
+import { Principal as LegacyPrincipal } from '@dfinity/principal-legacy';
 import fetch from 'isomorphic-fetch';
-
-import { HttpAgent } from '..';
 
 describe('compatibility with old actor', () => {
   it('should register successfully', async () => {
     console.log('foo');
 
-    const agent = await new HttpAgent({ fetch, host: 'http:localhost:8000' }).toLegacyAgent();
+    const agent = await new LegacyHttpAgent({
+      fetch,
+      host: 'http:localhost:8000',
+    });
     const actor = getNFTActor({
       canisterId: 'aaa-aaa',
       agent,
       standard: 'ext',
     });
     expect(actor).toBeTruthy();
+  });
+});
+
+describe('principal compatibility', () => {
+  it('should support from ', () => {
+    LegacyPrincipal.anonymous();
+    const test = Principal.from(LegacyPrincipal.anonymous());
+    expect(test).toMatchInlineSnapshot();
   });
 });
