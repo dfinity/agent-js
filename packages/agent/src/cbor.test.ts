76df16ec74da978a24e095b56be0a71b4be3ce58
neeboo <ghostcorn@gmail.com>
2022-09-04 14:12:24 +0800

    :white_check_mark: adding test to cbor

--- packages/agent/src/cbor.test.ts
+++ packages/agent/src/cbor.test.ts
@@ -54,6 +54,82 @@ test('empty canister ID', () => {
   expect(Principal.fromUint8Array(outputA as any).toText()).toBe('aaaaa-aa');
 });
 
+describe('cbor encode/decode bigint', () => {
+  test('encode decode 0 to 1_000_000', () => {
+    for (let i = 0; i < 1_000_000; i += 1) {
+      let buffer;
+
+      try {
+        buffer = encode(BigInt(i));
+      } catch (error) {
+        console.log(`error encoding on ${i.toString()}`);
+        expect((error as Error).message).toBeTruthy();
+      }
+
+      try {
+        const decoded = decode<bigint>(buffer);
+        expect(decoded).toBe(BigInt(i));
+      } catch (error) {
+        console.log(`error decoding on ${i.toString()}`);
+        expect((error as Error).message).toBeTruthy();
+      }
+      // if (i % 100000 === 0) {
+      //   console.log(`${new Date(Date.now()).toLocaleTimeString()} with ${i.toString()}`);
+      // }
+    }
+  });
+  test('encode decode 1_000_000n to 1_000_000_000_000n', () => {
+    for (let i = 1_000_000; i < 1_000_000_000_000; i += 1_000_000) {
+      let buffer;
+
+      const random_num = Math.floor(Math.random() * Math.pow(10, i.toString().length - 1));
+
+      const actual = BigInt(i + random_num);
+      try {
+        buffer = encode(actual);
+      } catch (error) {
+        console.log(`error encoding on ${i.toString()}`);
+        expect((error as Error).message).toBeTruthy();
+      }
+
+      try {
+        const decoded = decode<bigint>(buffer);
+        expect(decoded).toBe(actual);
+      } catch (error) {
+        console.log(`error decoding on ${i.toString()}`);
+        expect((error as Error).message).toBeTruthy();
+      }
+      // if (i % 1_000_000_000_00 === 0) {
+      //   console.log(`${new Date(Date.now()).toLocaleTimeString()} with ${actual.toString()}`);
+      // }
+    }
+  });
+  test('encode decode 1_000_000_000_000n to 1_000_000_000_000_000_000n', () => {
+    for (let i = 1_000_000_000_000n; i < 1_000_000_000_000_000_000n; i += 1_000_000_000_000n) {
+      let buffer;
+      const random_num = BigInt(Math.floor(Math.random() * Math.pow(10, i.toString().length - 1)));
+      const actual = i + random_num;
+      try {
+        buffer = encode(actual);
+      } catch (error) {
+        console.log(`error encoding on ${i.toString()}`);
+        expect((error as Error).message).toBeTruthy();
+      }
+
+      try {
+        const decoded = decode<bigint>(buffer);
+        expect(decoded).toBe(actual);
+      } catch (error) {
+        console.log(`error decoding on ${i.toString()}`);
+        expect((error as Error).message).toBeTruthy();
+      }
+      // if (i % 1_000_000_000_000_000_00n === 0n) {
+      //   console.log(`${new Date(Date.now()).toLocaleTimeString()} with ${actual.toString()}`);
+      // }
+    }
+  });
+});
+
 function buf2hex(buffer: Uint8Array) {
   // Construct an array such that each number is translated to the
   // hexadecimal equivalent, ensure it is a string and padded then
