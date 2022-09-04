9bc956832379613e86578d6d36cb6b7bcd730cea
neeboo <ghostcorn@gmail.com>
2022-09-04 14:17:29 +0800

    fix: adding test to cbor

--- packages/agent/src/cbor.test.ts
+++ packages/agent/src/cbor.test.ts
@@ -73,6 +73,7 @@ describe('cbor encode/decode bigint', () => {
         console.log(`error decoding on ${i.toString()}`);
         expect((error as Error).message).toBeTruthy();
       }
+      // // if we log the time
       // if (i % 100000 === 0) {
       //   console.log(`${new Date(Date.now()).toLocaleTimeString()} with ${i.toString()}`);
       // }
@@ -99,6 +100,7 @@ describe('cbor encode/decode bigint', () => {
         console.log(`error decoding on ${i.toString()}`);
         expect((error as Error).message).toBeTruthy();
       }
+      // // if we log the time
       // if (i % 1_000_000_000_00 === 0) {
       //   console.log(`${new Date(Date.now()).toLocaleTimeString()} with ${actual.toString()}`);
       // }
@@ -123,6 +125,7 @@ describe('cbor encode/decode bigint', () => {
         console.log(`error decoding on ${i.toString()}`);
         expect((error as Error).message).toBeTruthy();
       }
+      // // if we log the time
       // if (i % 1_000_000_000_000_000_00n === 0n) {
       //   console.log(`${new Date(Date.now()).toLocaleTimeString()} with ${actual.toString()}`);
       // }
