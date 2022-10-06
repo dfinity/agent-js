7260e1af48c310ebbe4ead794fd4c01d25e90f9e
sea-snake <sea-snake@outlook.com>
2022-10-06 02:31:09 +0200

    feat: Increase file sizes in E2E assets tests.

--- e2e/node/basic/assets.test.ts
+++ e2e/node/basic/assets.test.ts
@@ -86,9 +86,9 @@ describe('assets', () => {
     }
   });
 
-  it('store, get and delete 1KB asset (single chunk)', () => testRandomBytes('1KB.bin', 1000));
+  it('store, get and delete 1MB asset (single chunk)', () => testRandomBytes('1MB.bin', 1000000));
 
-  it('store, get and delete 3KB asset (multiple chunk)', () => testRandomBytes('3KB.bin', 3000));
+  it('store, get and delete 3MB asset (multiple chunk)', () => testRandomBytes('3MB.bin', 3000000));
 
   it('batch process assets and verify asset list', async () => {
     const assetManager = new AssetManager({ canisterId, agent: await agent });
