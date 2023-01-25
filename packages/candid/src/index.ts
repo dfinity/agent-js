e1d8d2def847f6cc72d996f833a00bb39f4ca202
Kyle Peacock <kylpeacock@gmail.com>
2023-01-25 14:12:02 -0800

    IDL replaced

--- packages/candid/src/index.ts
+++ packages/candid/src/index.ts
@@ -1,6 +1,6 @@
 export * from './candid-ui';
 export * from './candid-core';
-export * as IDL from './idl';
+export { IDL } from '@dfinity/types';
 export * from './utils/hash';
 export * from './utils/leb128';
 export * from './utils/buffer';
