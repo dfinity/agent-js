79a417bb2f67a713ba68c841802c75769728a389
Kepler Vital <mail@keplervital.com>
2023-07-07 15:25:59 +0200

    feat: adds method for actor creation that includes boundary node http details

--- packages/agent/src/actor.ts
+++ packages/agent/src/actor.ts
@@ -2,6 +2,7 @@ import { Buffer } from 'buffer/';
 import {
   Agent,
   getDefaultAgent,
+  HttpDetailsResponse,
   QueryResponseRejected,
   QueryResponseStatus,
   ReplicaRejectCode,
@@ -145,10 +146,29 @@ export type ActorSubclass<T = Record<string, ActorMethod>> = Actor & T;
 /**
  * An actor method type, defined for each methods of the actor service.
  */
-export interface ActorMethod<Args extends unknown[] = unknown[], Ret extends unknown = unknown> {
+export interface ActorMethod<Args extends unknown[] = unknown[], Ret = unknown> {
   (...args: Args): Promise<Ret>;
   withOptions(options: CallConfig): (...args: Args) => Promise<Ret>;
 }
+
+/**
+ * An actor method type, defined for each methods of the actor service.
+ */
+export interface ActorMethodWithHttpDetails<Args extends unknown[] = unknown[], Ret = unknown>
+  extends ActorMethod {
+  (...args: Args): Promise<{ httpDetails: HttpDetailsResponse; result: Ret }>;
+}
+
+export type FunctionWithArgsAndReturn<Args extends unknown[] = unknown[], Ret = unknown> = (
+  ...args: Args
+) => Ret;
+
+// Update all entries of T with the extra information from ActorMethodWithInfo
+export type ActorMethodMappedWithHttpDetails<T> = {
+  [K in keyof T]: T[K] extends FunctionWithArgsAndReturn<infer Args, infer Ret>
+    ? ActorMethodWithHttpDetails<Args, Ret>
+    : never;
+};
 
 /**
  * The mode used when installing a canister.
@@ -251,7 +271,10 @@ export class Actor {
     return this.createActor(interfaceFactory, { ...config, canisterId });
   }
 
-  public static createActorClass(interfaceFactory: IDL.InterfaceFactory): ActorConstructor {
+  public static createActorClass(
+    interfaceFactory: IDL.InterfaceFactory,
+    withHttpDetails = false,
+  ): ActorConstructor {
     const service = interfaceFactory({ IDL });
 
     class CanisterActor extends Actor {
@@ -273,6 +296,10 @@ export class Actor {
         });
 
         for (const [methodName, func] of service._fields) {
+          if (withHttpDetails) {
+            func.annotations.push(ACTOR_METHOD_WITH_HTTP_DETAILS);
+          }
+
           this[methodName] = _createActorMethod(this, methodName, func, config.blsVerify);
         }
       }
@@ -285,9 +312,18 @@ export class Actor {
     interfaceFactory: IDL.InterfaceFactory,
     configuration: ActorConfig,
   ): ActorSubclass<T> {
-    return new (this.createActorClass(interfaceFactory))(
+    return new (this.createActorClass(interfaceFactory, false))(
       configuration,
     ) as unknown as ActorSubclass<T>;
+  }
+
+  public static createActorWithHttpDetails<T = Record<string, ActorMethod>>(
+    interfaceFactory: IDL.InterfaceFactory,
+    configuration: ActorConfig,
+  ): ActorSubclass<ActorMethodMappedWithHttpDetails<T>> {
+    return new (this.createActorClass(interfaceFactory, true))(
+      configuration,
+    ) as unknown as ActorSubclass<ActorMethodMappedWithHttpDetails<T>>;
   }
 
   private [metadataSymbol]: ActorMetadata;
@@ -318,6 +354,8 @@ const DEFAULT_ACTOR_CONFIG = {
 
 export type ActorConstructor = new (config: ActorConfig) => ActorSubclass;
 
+export const ACTOR_METHOD_WITH_HTTP_DETAILS = 'http-details';
+
 function _createActorMethod(
   actor: Actor,
   methodName: string,
@@ -347,7 +385,12 @@ function _createActorMethod(
           throw new QueryCallRejectedError(cid, methodName, result);
 
         case QueryResponseStatus.Replied:
-          return decodeReturnValue(func.retTypes, result.reply.arg);
+          return func.annotations.includes(ACTOR_METHOD_WITH_HTTP_DETAILS)
+            ? {
+                httpDetails: result.httpDetails,
+                result: decodeReturnValue(func.retTypes, result.reply.arg),
+              }
+            : decodeReturnValue(func.retTypes, result.reply.arg);
       }
     };
   } else {
@@ -382,11 +425,22 @@ function _createActorMethod(
 
       const pollStrategy = pollingStrategyFactory();
       const responseBytes = await pollForResponse(agent, ecid, requestId, pollStrategy, blsVerify);
+      const shouldIncludeHttpDetails = func.annotations.includes(ACTOR_METHOD_WITH_HTTP_DETAILS);
 
       if (responseBytes !== undefined) {
-        return decodeReturnValue(func.retTypes, responseBytes);
+        return shouldIncludeHttpDetails
+          ? {
+              httpDetails: response,
+              result: decodeReturnValue(func.retTypes, responseBytes),
+            }
+          : decodeReturnValue(func.retTypes, responseBytes);
       } else if (func.retTypes.length === 0) {
-        return undefined;
+        return shouldIncludeHttpDetails
+          ? {
+              httpDetails: response,
+              result: undefined,
+            }
+          : undefined;
       } else {
         throw new Error(`Call was returned undefined, but type [${func.retTypes.join(',')}].`);
       }
