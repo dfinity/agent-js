e21169bc7be0bdbc75f63071a095a2c3d3aee05c
Kyle Peacock <kylpeacock@gmail.com>
2023-10-26 12:42:32 -0700

    removing drift from calculation before rounding

--- packages/agent/src/agent/http/transforms.ts
+++ packages/agent/src/agent/http/transforms.ts
@@ -18,9 +18,7 @@ export class Expiry {
 
   constructor(deltaInMSec: number) {
     // Use bigint because it can overflow the maximum number allowed in a double float.
-    const raw_value =
-      (BigInt(Date.now()) + BigInt(deltaInMSec) - REPLICA_PERMITTED_DRIFT_MILLISECONDS) *
-      NANOSECONDS_PER_MILLISECONDS;
+    const raw_value = (BigInt(Date.now()) + BigInt(deltaInMSec)) * NANOSECONDS_PER_MILLISECONDS;
 
     // round down to the nearest second
     const ingress_as_seconds = raw_value / BigInt(1_000_000_000);
