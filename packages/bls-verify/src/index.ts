184bd86ff03c8ef053507238ce9b8b2cdda56ff9
Kyle Peacock <kylpeacock@gmail.com>
2023-11-08 12:21:50 -0800

    wip

--- packages/bls-verify/src/index.ts
+++ packages/bls-verify/src/index.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { CTX } from 'amcl-js';
 import { toHex } from './utils';
+import { bls12_381 as bls } from '@noble/curves/bls12-381';
 
 export const blsVerify = async (
   primaryKey: Uint8Array | string,
@@ -10,16 +9,5 @@ export const blsVerify = async (
   const pk = typeof primaryKey === 'string' ? primaryKey : toHex(primaryKey);
   const sig = typeof signature === 'string' ? signature : toHex(signature);
   const msg = typeof message === 'string' ? message : toHex(message);
-  const ctx = new CTX('BLS12381');
-  if (((ctx as any).BLS as any).init() !== 0) {
-    throw new Error('Cannot initialize BLS');
-  }
-
-  return (
-    ((ctx as any).BLS as any).core_verify(
-      ((ctx as any).BLS as any).stringtobytes(sig),
-      ((ctx as any).BLS as any).stringtobytes(msg),
-      ((ctx as any).BLS as any).stringtobytes(pk),
-    ) == 0
-  );
+  return bls.verify(sig, msg, pk);
 };
--- packages/bls-verify/src/vendor/amcl-js/index.js
+++
@@ -1,1 +0,0 @@
-module.exports.CTX = require('./src/ctx');
