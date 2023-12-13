683c8f55446da42efa63a39f414b69d6120099c3
Kyle Peacock <kylpeacock@gmail.com>
2023-12-13 11:19:38 -0800

    updating @noble/hashes

--- packages/bls-verify/src/index.ts
+++ packages/bls-verify/src/index.ts
@@ -1,13 +1,12 @@
+import { bls12_381 } from '@noble/curves/bls12-381';
 import { toHex } from './utils';
 export const blsVerify = async (
   primaryKey: Uint8Array | string,
   signature: Uint8Array | string,
   message: Uint8Array | string,
 ): Promise<boolean> => {
-  const { verifyShortSignature } = (await import('./verifyShortSignature')).default() as any;
-
   const pk = typeof primaryKey === 'string' ? primaryKey : toHex(primaryKey);
   const sig = typeof signature === 'string' ? signature : toHex(signature);
   const msg = typeof message === 'string' ? message : toHex(message);
-  return verifyShortSignature(sig, msg, pk);
+  return bls12_381.verifyShortSignature(sig, msg, pk);
 };
