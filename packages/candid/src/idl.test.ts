9ae1132312c6e3fbb1c3833e9cd7d9b86d549d78
Kyle Peacock <kylpeacock@gmail.com>
2024-01-11 09:58:56 -0800

    invalid JSDoc tag name "jest-environment"

--- packages/candid/src/idl.test.ts
+++ packages/candid/src/idl.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/**
+/*
  * @jest-environment node
  */
 import * as IDL from './idl';
