6844ec2407e813421d1d6b9603c4c207d3a54c65
Kyle Peacock <kylpeacock@gmail.com>
2024-01-23 15:16:28 -0800

    fix: export partial identity from index of @dfinity/identity

--- packages/identity/src/index.ts
+++ packages/identity/src/index.ts
@@ -1,6 +1,7 @@
 export { Ed25519KeyIdentity, Ed25519PublicKey } from './identity/ed25519';
 export * from './identity/ecdsa';
 export * from './identity/delegation';
+export * from './identity/partial';
 export { WebAuthnIdentity } from './identity/webauthn';
 export { wrapDER, unwrapDER, DER_COSE_OID, ED25519_OID } from '@dfinity/agent';
 
