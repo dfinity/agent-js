97f95321130cd4aa8d4a5f4973baa069a5d2a3ab
Hamish Peebles <hamishpeebles@gmail.com>
2024-02-15 14:18:04 +0000

    feat: export `AuthClientStorage` in order to support custom implementations

--- packages/auth-client/src/index.ts
+++ packages/auth-client/src/index.ts
@@ -28,7 +28,7 @@ import {
 } from './storage';
 import { PartialIdentity } from '@dfinity/identity/lib/cjs/identity/partial';
 
-export { IdbStorage, LocalStorage, KEY_STORAGE_DELEGATION, KEY_STORAGE_KEY } from './storage';
+export { AuthClientStorage, IdbStorage, LocalStorage, KEY_STORAGE_DELEGATION, KEY_STORAGE_KEY } from './storage';
 export { IdbKeyVal, DBCreateOptions } from './db';
 
 const IDENTITY_PROVIDER_DEFAULT = 'https://identity.ic0.app';
