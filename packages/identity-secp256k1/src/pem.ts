29692f029a5e171908cfdc22eb5ee42e2adab71c
Kyle Peacock <kylpeacock@gmail.com>
2024-02-16 14:14:48 -0800

    feat: check for ASN.1 OID before returning private key

--- packages/identity-secp256k1/src/pem.ts
+++ packages/identity-secp256k1/src/pem.ts
@@ -1,5 +1,4 @@
-import { concat, uint8ToBuf } from '@dfinity/agent';
-import asn1js from 'asn1js';
+import * as asn1js from 'asn1js';
 
 const HEADER = `-----BEGIN EC PRIVATE KEY-----`;
 const FOOTER = `-----END EC PRIVATE KEY-----`;
@@ -25,75 +24,17 @@ export function pemToSecretKey(pem: string): ArrayBuffer {
   const base64Data = lines.slice(1, -1).join('').replace(/\r?\n/g, '');
   const rawKey = Uint8Array.from(atob(base64Data), c => c.charCodeAt(0));
 
-  /*
-   * Rawkey contents:
-   * 0:d=0  hl=3 l= 132 cons: SEQUENCE
-   * 3:d=1  hl=2 l=   1 prim: INTEGER           :00
-   * 6:d=1  hl=2 l=  16 cons: SEQUENCE
-   * 8:d=2  hl=2 l=   7 prim: OBJECT            :id-ecPublicKey
-   * 17:d=2  hl=2 l=   5 prim: OBJECT            :secp256k1
-   * 24:d=1  hl=2 l= 109 prim: OCTET STRING      [HEX DUMP]:....
-   */
-  parseSequence(rawKey); //?
-  asn1js.fromBER(rawKey); //?
-  return rawKey.slice(7, 39);
-}
-
-interface Asn1Node {
-  type: number;
-  value: Uint8Array | Asn1Node[];
-}
-
-function parseAsn1(data: Uint8Array): Asn1Node | null {
-  let offset = 0;
-
-  if (data.length < 2) {
-    return null;
-  }
-
-  const type = data[offset++];
-  let length = data[offset++];
-
-  if (length & 0x80) {
-    // Long form length
-    const numLengthBytes = length & 0x7f;
-    if (data.length < offset + numLengthBytes + length) {
-      return null;
+  // Parse the key as ASN.1 object
+  try {
+    const asn1 = asn1js.fromBER(rawKey);
+    // OID for secp256k1
+    if (!asn1.result.toString().includes(`OBJECT IDENTIFIER : 1.3.132.0.10`)) {
+      throw new Error('Invalid curve, must be secp256k1');
     }
-    length = 0;
-    for (let i = 0; i < numLengthBytes; i++) {
-      length = (length << 8) | data[offset++];
-    }
+    // Returns the 32-byte raw private key at the appropriate offset
+    return rawKey.slice(7, 39);
+  } catch (error) {
+    console.error(error);
+    throw new Error('Encountered error while parsing PEM key');
   }
-
-  if (data.length < offset + length) {
-    return null;
-  }
-
-  const value: Asn1Node = { type, value: new Uint8Array(length) };
-  data.copyWithin(offset, offset + length, offset + length + offset);
-
-  if (type === 0x30) {
-    // Constructed type (sequence)
-    value.value = parseSequence(value.value as Uint8Array);
-  }
-
-  return value;
-}
-
-function parseSequence(data: Uint8Array): Asn1Node[] {
-  const nodes: Asn1Node[] = [];
-  let offset = 0;
-
-  while (offset < data.length) {
-    const node = parseAsn1(data.slice(offset));
-    if (node) {
-      nodes.push(node);
-      offset += (node.value as Uint8Array).byteLength + 2;
-    } else {
-      break;
-    }
-  }
-
-  return nodes;
 }
