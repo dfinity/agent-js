b623f0d28a8f3239f067bc05eba2874a02a382a3
Claudio Russo <claudio@dfinity.org>
2025-03-21 20:29:40 +0100

    fix: make IDL/Candid decoding of options spec compliant (#981)
    * add backtracking to opt decoding
    
    * basic tests of variant defaulting under opt
    
    * implement remaining variant decoding edge cases
    
    * test opt parsing of null and any edge cases
    
    * add test for non-opt cases
    
    Co-authored-by: Kaia Peacock <kaia@peacock.dev>

--- packages/candid/src/utils/buffer.ts
+++ packages/candid/src/utils/buffer.ts
@@ -39,6 +39,21 @@ export class PipeArrayBuffer {
   private _view: Uint8Array;
 
   /**
+   * Save a checkpoint of the reading view (for backtracking)
+   */
+  public save() : Uint8Array {
+     return this._view
+  }
+
+  /**
+   * Restore a checkpoint of the reading view (for backtracking)
+   * @param checkPoint a previously saved checkpoint
+   */
+  public restore(checkPoint: Uint8Array) {
+     this._view = checkPoint
+  }
+
+  /**
    * The actual buffer containing the bytes.
    * @private
    */
