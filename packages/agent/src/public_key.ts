8afe147cb5d0dddc0b900bc935434789d361f939
ilbertt <luca.bertelli@dfinity.org>
2025-04-22 12:47:34 +0200

    feat: der error on public key

--- packages/agent/src/public_key.ts
+++ packages/agent/src/public_key.ts
@@ -1,5 +1,6 @@
 import { DerEncodedPublicKey, PublicKey } from './auth';
 import { ED25519_OID, unwrapDER, wrapDER } from './der';
+import { DerDecodeError, ErrorKind } from './errors';
 
 export class Ed25519PublicKey implements PublicKey {
   public static from(key: PublicKey): Ed25519PublicKey {
@@ -24,7 +25,10 @@ export class Ed25519PublicKey implements PublicKey {
   private static derDecode(key: DerEncodedPublicKey): ArrayBuffer {
     const unwrapped = unwrapDER(key, ED25519_OID);
     if (unwrapped.length !== this.RAW_KEY_LENGTH) {
-      throw new Error('An Ed25519 public key must be exactly 32bytes long');
+      throw new DerDecodeError(
+        { error: 'An Ed25519 public key must be exactly 32 bytes long' },
+        ErrorKind.Input,
+      );
     }
     return unwrapped;
   }
@@ -44,7 +48,10 @@ export class Ed25519PublicKey implements PublicKey {
   // `fromRaw` and `fromDer` should be used for instantiation, not this constructor.
   private constructor(key: ArrayBuffer) {
     if (key.byteLength !== Ed25519PublicKey.RAW_KEY_LENGTH) {
-      throw new Error('An Ed25519 public key must be exactly 32bytes long');
+      throw new DerDecodeError(
+        { error: 'An Ed25519 public key must be exactly 32 bytes long' },
+        ErrorKind.Input,
+      );
     }
     this.#rawKey = key;
     this.#derKey = Ed25519PublicKey.derEncode(key);
