1d72033e919ee8f2d91cc7a9c1cdf1424e1e62f8
ilbertt <luca.bertelli@dfinity.org>
2025-05-28 18:49:44 +0200

    test: fix e2e tests tsconfig

--- e2e/node/vitest.config.ts
+++ e2e/node/vitest.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   test: {
     setupFiles: ['./test-setup.ts'],
     testTimeout: 100_000,
+    typecheck: {
+      enabled: true,
+      tsconfig: './tsconfig.test.json',
+    },
   },
 });
