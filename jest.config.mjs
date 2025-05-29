f0b13a54d044fe95840cbc7db260c8e2b2ff3113
ilbertt <luca.bertelli@dfinity.org>
2025-05-29 15:03:38 +0200

    refactor: explain .mjs extension in comment

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,7 @@
 // Using .ts files for Jest configs gives a lot of problems, especially related to ts-node.
 // This issue gives an overview of the problems: https://github.com/jestjs/jest/issues/11453, even if it's closed.
-// For this reason, we're using .js files instead.
+// For this reason, we're using .mjs files instead.
+// We need the .mjs extension to make sure that the file is parsed as a module in all the Node.js versions we support.
 
 import { baseConfig } from './jest.config.base.js';
 
