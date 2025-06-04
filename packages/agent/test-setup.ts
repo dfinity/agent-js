6244d8eff7dab2d9d267e0655aade358c2409329
NathanosDev <contact@nathanos.dev>
2025-06-04 16:40:50 +0200

    remove redundant (and broken) tests

--- packages/agent/test-setup.ts
+++ packages/agent/test-setup.ts
@@ -8,14 +8,14 @@
 // Node.js in a similar way.
 
 import { Crypto } from '@peculiar/webcrypto';
-global.crypto = new Crypto();
-import { TextEncoder } from 'text-encoding';
-global.TextEncoder = TextEncoder;
-import { TextDecoder } from 'text-encoding';
-global.TextDecoder = TextDecoder;
-
+import { TextDecoder, TextEncoder } from 'text-encoding';
 import fetch from 'isomorphic-fetch';
+
+global.crypto = new Crypto();
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 global.fetch = fetch;
+
 Object.defineProperty(global, 'performance', {
   writable: true,
   value: { ...global.performance },
