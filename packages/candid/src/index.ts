e74a68c5beb8bd6d06e2aa77fd614210b7dc1fdb
ilbertt <luca.bertelli@dfinity.org>
2025-07-17 12:27:16 +0200

    Revert "refactor: remove extension from relative import to make the CI fail"
    This reverts commit 46057a2fdd3eecb13ac42bf66386dd08e389d060.

--- packages/candid/src/index.ts
+++ packages/candid/src/index.ts
@@ -1,6 +1,6 @@
 export * from './candid-ui.ts';
 export * from './candid-core.ts';
-export * as IDL from './idl';
+export * as IDL from './idl.ts';
 export * from './utils/hash.ts';
 export * from './utils/leb128.ts';
 export * from './utils/buffer.ts';
--- packages/identity-secp256k1/src/index.ts
+++ packages/identity-secp256k1/src/index.ts
@@ -1,1 +1,1 @@
-export * from './secp256k1';
+export * from './secp256k1.ts';
