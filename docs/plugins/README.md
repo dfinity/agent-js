52d25d97e4e3fadce30eec8eb2e5eba16537bd49
Nathan Mc Grath <contact@nathanos.dev>
2025-08-18 14:34:13 +0200

    refactor: break libs plugin into additional files, and markdown urls plugins (#1101)

---
+++ docs/plugins/README.md
@@ -0,0 +1,9 @@
+# DFINITY Starlight Plugins
+
+## Overview
+
+Starlight plugins for use in DFINITY documentation websites.
+
+## License
+
+This project is licensed under the [Apache-2.0 License](LICENSE).
---
+++ docs/plugins/src/utils/constants.ts
@@ -0,0 +1,5 @@
+import path from "node:path";
+
+export const CONTENT_DIR = path.join("src", "content");
+export const TMP_DIR = path.join(CONTENT_DIR, "tmp");
+export const DOCS_DIR = path.join(CONTENT_DIR, "docs");
